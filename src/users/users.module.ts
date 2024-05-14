58e2965bd8efa844359c8adab7007d7d860a2b80
Tal Kohavy <talkohavy@gmail.com>
2024-05-14 12:43:20 +0300

    possible export of a provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,8 +3,9 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [],
   controllers: [UsersController],
   providers: [UsersService],
+  // exports: [UsersService], // <--- Every module is automatically a shared module. Once created it can be reused by any module. Let's imagine that we want to share an instance of the UsersService between several other modules. In order to do that, we first need to export the UsersService provider by adding it to the module's exports array, as shown here. Now any module that imports the UsersModule has access to the UsersService and will share the same instance with all other modules that import it as well.
+  imports: [],
 })
 export class UsersModule {}
